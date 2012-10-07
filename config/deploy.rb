d66a85a124b84c14e724b348d9cc3918d82914cb
Jan Bernacki <releu@me.com>
2012-10-07 10:25:26 +0400

    fix config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
 require 'bundler/capistrano'
 require "rvm/capistrano"
-require 'capistrano-resque'
 
 set :rvm_type, :system
 set :rvm_ruby_string, '1.9.3@notifier-server'
 
 set :application, "notifier-server"
-set :domain, "gistflow.com"
+set :domain, "git@gistflow.com"
 set :repository,  "git://github.com/gistflow/notifier-server.git"
 set :branch, "master"
 set :use_sudo, false
@@ -18,7 +17,7 @@ role :web, domain
 
 namespace :deploy do
   task :restart do
-    run "kill -9 `cat #{deploy_to}/tmp/azazel.pid`"
+    run "kill -9 `cat #{deploy_to}/tmp/azazel.pid`" rescue nil
     run "cd #{release_path} && bundle exec ruby server.rb"
   end
 end
